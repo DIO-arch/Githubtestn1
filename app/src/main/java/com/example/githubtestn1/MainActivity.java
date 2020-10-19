d5a407a1a2b16f4344afce6bfdcb61a30ab36e78
DIO-arch <simaassaarf@gmail.com>
2020-10-19 22:16:32 +0300

    Initial commit

---
+++ app/src/main/java/com/example/githubtestn1/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.githubtestn1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+int x=0;
+TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv=findViewById(R.id.button);
+    }
+
+    public void methodToDo(View view) {
+        x++;
+        String st=getString(R.string.textOnTextBox);
+        tv.setText(st + " " + x);
+    }
+}
